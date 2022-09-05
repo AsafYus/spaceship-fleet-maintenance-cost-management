da314f6751b78b1e15c1aca49132e73fece7a611
Asaf Yusufov <ayosopov@gmail.com>
2022-09-05 16:54:32 +0300

    Added the contents of Spacecraft.h and Spacecraft.cpp

--- Spacecraft.cpp
+++ Spacecraft.cpp
@@ -1,15 +1,63 @@
 //  file Spacecraft.cpp
 #include "Spacecraft.h"
+#include <iostream>
 
+Spacecraft::Spacecraft() : fire_power(10) {}
 
-
-
-void Spacecraft::set_maximal_speed(float maximalSpeed)
+void Spacecraft::set_name(const std::string& _name)
 {
-    if (maximalSpeed >= 0.0f && maximalSpeed <= 10.0f)
-        this->maximalSpeed = maximalSpeed;
-    else
-        this->maximalSpeed = 0.0f;
+    auto search = names.find(_name);
+    if (search != names.end())
+        std::cout << "Error!! The database already have a spacecraft with that name" << std::endl;
+    else {
+        name = _name;
+        names.insert(name);
+    }
 }
 
+std::string Spacecraft::get_name()
+{
+    return name;
+}
+
+void Spacecraft::set_comission_year(int _commission_year)
+{
+    commission_year = _commission_year;
+}
+
+int Spacecraft::get_commission_year()
+{
+    return commission_year;
+}
+
+void Spacecraft::set_maximal_speed(float _maximal_speed)
+{
+    if (_maximal_speed >= 0.0f && _maximal_speed <= 10.0f)
+        maximal_speed = _maximal_speed;
+    else
+        maximal_speed = 0.0f;
+}
+
+float Spacecraft::get_maximal_speed()
+{
+    return maximal_speed;
+}
+
+void Spacecraft::set_cargo_capacity(int _cargo_capacity)
+{
+    if (_cargo_capacity > 0)
+        cargo_capacity = _cargo_capacity;
+    else
+        cargo_capacity = 0;
+}
+
+int Spacecraft::get_cargo_capacity()
+{
+    return cargo_capacity;
+}
+
+int Spacecraft::get_fire_power()
+{
+    return fire_power;
+}
 
--- Spacecraft.h
+++ Spacecraft.h
@@ -4,23 +4,53 @@
 #define SPACECRAFT_H
 
 #include <string>
+#include <unordered_set>
 
 class Spacecraft {    
 public:
-    std::string name;
-    int commissionYear, cargoCapacit, firePower;
-    float maximalSpeed;
     
-    Spacecraft() : firePower(10) {
-    }
-    
-    void set_maximal_speed(float maximalSpeed);
-
-    //  A method that returns a total annual maintenance cost.
-    virtual int getAnnualMaintenanceCost() = 0;
+    Spacecraft();
 
     virtual ~Spacecraft() = default;
 
+    // Sets the unique name of spaceship.
+    void set_name(const std::string& _name);
+    // Returns the unique name of spaceship.
+    std::string get_name();
+    
+    // Sets the commission Year.
+    void set_comission_year(int _commission_year);
+    // Returns the year of manufacture spaceship.
+    int get_commission_year();
+
+    // Sets the maximum speed of spaceship.
+    void set_maximal_speed(float _maximal_speed);
+    // Returns the maximum speed of spaceship.
+    float get_maximal_speed();
+
+    // Sets the cargo capacity of spaceship.
+    void set_cargo_capacity(int _cargo_capacity);
+    // Gets the cargo capacity of spaceship.
+    int get_cargo_capacity();
+
+    // Gets the fire power of spaceship.
+    virtual int get_fire_power();
+    
+    // A method that returns a total annual maintenance cost.
+    virtual int get_annual_maintenance_cost() = 0;
+    
+    
+    
+
+    
+    
+
+private:
+    std::string name;
+    int commission_year, cargo_capacity, fire_power;
+    float maximal_speed;
+
+    std::unordered_set<std::string> names;  // The unique names of the spaceships.
 };
 
 #endif  // SPACECRAFT_H
